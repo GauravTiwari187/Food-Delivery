08a5f1400c971f89018a9a9700922edf03924860
GauravTiwari187 <gaurav2489tiwari@gmail.com>
2026-07-07 18:18:58 +0530

    Fix auth JWT secret

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontent
 const placeorder = async (req,res) =>{
 
-    const frontend_url = "http://localhost:5173"
+   const frontend_url = "https://food-delivery-two-black.vercel.app"
   
     try {
         const newOrder = new orderModel({
