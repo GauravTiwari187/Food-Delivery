a18389ae58326f3aea1f54c79f96706bb974b0d2
GauravTiwari187 <gaurav2489tiwari@gmail.com>
2026-07-08 21:04:40 +0530

    Fix MongoDB password

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     await mongoose.connect(
-        'mongodb+srv://GauravTiwari148:Gaurav12345@cluster0.hfotbzs.mongodb.net/food-del'
+        'mongodb+srv://GauravTiwari148:Gaurav1234@cluster0.hfotbzs.mongodb.net/food-del'
     );
     console.log("DB Connected ✅");
 }
